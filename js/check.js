bdccf6dfe50f5380a70218e82c54f5a9c034eee6
Uqoug <gq280391457@gmail.com>
2017-01-06 23:15:37 +0800

    0106

--- js/check.js
+++ js/check.js
@@ -1,27 +1,39 @@
 requirejs.config({
-  paths: {
-    jquery: "jquery-3.1.1",
-    dataHandler: "dataHandler"
+    paths: {
+        jquery: "jquery-3.1.1",
+        dataHandler: "dataHandler",
+        echart: "echarts"
   }
 });
 
-requirejs(["jquery", "dataHandler"], function ($, dataHandler){
+requirejs(["jquery", "dataHandler", "echarts"],
+function ($, dataHandler, echarts){
     var handler = new dataHandler.dataHandler;
     var storage = window.localStorage;
     var num = location.search
               .substring(5, location.search.length);
     var ddataJSON = storage.getItem('data' + num);
     var ddata = JSON.parse(ddataJSON);
-    ObjLength = function(obj) {
+    console.log(ddata);
+    $(".title").text(ddata.title);
+    function getRandom() {
+        return Math.floor(Math.random() * 100);
+    };
+    objLength = function(obj) {
         var count = 0;
         for(var i in obj){
             count ++;
         }
         return count;
     };
-    for (var i = 1; i <= ObjLength(ddata.question); i++) {
-    	if (ddata.question["Q" + i]["type"] === "radio") {
+    for (var i = 1; i <= objLength(ddata.question); i++) {
+    	if (ddata.question["Q" + i]["type"] == "radio") {
             handler.radio(i - 1);
+        }
+        else if (ddata.question["Q" + i]["type"] == "checkbox") {
+            handler.checkbox(i - 1);
         };
     };
+
+    
 });
--- js/dataHandler.js
+++ js/dataHandler.js
@@ -1,4 +1,4 @@
-define(['jquery'], function ($) {
+define(['jquery', 'echarts'], function ($, echarts) {
     function dataHandler() {};
     
     var storage = window.localStorage;
@@ -7,27 +7,19 @@ define(['jquery'], function ($) {
     var ddataJSON = storage.getItem('data' + num);
     var ddata = JSON.parse(ddataJSON);
     $(".title").text(ddata.title);
-    
     function getRandom() {
         return Math.floor(Math.random() * 100);
     };
 
 
     dataHandler.prototype.radio = function(Qnum) {
-        objLength = function(obj) {
-            var count = 0;
-            for(var i in obj){
-                count ++;
-            }
-            return count;
-        };
-        $(".databox:hidden").clone()
+        $(".radiobox:hidden").clone()
         .removeAttr('hidden').appendTo($(".context"));
-        $(".databox:visible:eq("+Qnum+") .q-title")
+        $(".radiobox:visible:eq("+Qnum+") .q-title")
         .text(ddata.question["Q" + (Qnum + 1)]["title"]);
         var question = ddata.question["Q" + (Qnum + 1)];
-        var $barbox = $(".databox:visible:eq("+Qnum+") .barbox");
-        var $Sname = $(".databox:visible:eq("+Qnum+") .s-name");
+        var $barbox = $(".radiobox:visible:eq("+Qnum+") .barbox");
+        var $Sname = $(".radiobox:visible:eq("+Qnum+") .s-name");
         var Sum = 0;
         var num = [];
         var percent = '';
@@ -38,14 +30,70 @@ define(['jquery'], function ($) {
         for (var i = 1; i <= objLength(question["choosen"]); i++) {
             percent = Math.floor(num[i] / Sum *100) + "%";
             $Sname.text(question["choosen"]['s' + i]);
-            $(".databox:visible:eq("+Qnum+") .bar:eq(0)")
+            $(".radiobox:visible:eq("+Qnum+") .bar:eq(0)")
             .css("width", percent);
-            $(".databox:visible:eq("+Qnum+") .percent:eq(0)")
+            $(".radiobox:visible:eq("+Qnum+") .percent:eq(0)")
             .text(percent);
-            $barbox.clone().appendTo($(".databox:visible:eq("+Qnum+")"));
+            $barbox.clone().appendTo($(".radiobox:visible:eq("+Qnum+")"));
         };
     };
     
+    dataHandler.prototype.checkbox = function(Qnum) {
+        $(".checkbox:hidden").clone().removeAttr("hidden")
+        .appendTo($(".context"));
+        var cbData = [];
+        var question = ddata.question["Q" + (Qnum + 1)];
+        for (var i = 0; i < objLength(question.choosen); i++) {
+            var sData = {value: getRandom(),
+                        name: question.choosen["s" + (i + 1)]};
+            cbData.push(sData);
+            $(".checkbox:visible:eq(0) .q-title")
+            .text(ddata.question["Q2"]["title"]);
+            $(".checkbox:visible .s-name:eq(0)")
+            .clone().appendTo(".s-namelist");
+            $(".checkbox:visible .s-name:eq("+i+")")
+            .text(question.choosen["s" + (i + 1)]);
+        };
+        var lastCb = $(".checkbox:visible").length;
+        var myChart = echarts.init($(".pie").get(lastCb));
+            var option = {
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{b}: {c} ({d}%)"
+                },
+                legend: {
+                    right: '0px',
+                    orient: 'vertical'
+                },
+                series: [
+                    {
+                        type:'pie',
+                        radius: ['50%', '70%'],
+                        avoidLabelOverlap: false,
+                        label: {
+                            normal: {
+                                show: false,
+                                position: 'center'
+                            },
+                            emphasis: {
+                                show: true,
+                                textStyle: {
+                                    fontSize: '30',
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        },
+                        labelLine: {
+                            normal: {
+                                show: false
+                            }
+                        },
+                        data: cbData
+                    }
+                ]
+            };
+        myChart.setOption(option);
+    };
 
     return {
         dataHandler: dataHandler
